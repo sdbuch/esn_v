748ccaf40c3a83f9c8385d5d22397d27c3ddf4f2
hwagyesa <sambuchanan6@gmail.com>
2016-07-06 15:17:32 -0400

    add image directory, finalize mem demo files and add timing file sample

--- mem_demo/in_mem.v
+++ mem_demo/in_mem.v
@@ -86,7 +86,7 @@ module in_mem (
 		altsyncram_component.address_aclr_a = "NONE",
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
-		altsyncram_component.init_file = "coswave_fs64fsig_64pts.mif",
+		altsyncram_component.init_file = "sinwave_fs64fsig_64pts.mif",
 		altsyncram_component.intended_device_family = "Cyclone IV E",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
@@ -122,7 +122,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "IN_1"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "coswave_fs64fsig_64pts.mif"
+// Retrieval info: PRIVATE: MIFfilename STRING "sinwave_fs64fsig_64pts.mif"
 // Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "512"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "2"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -137,7 +137,7 @@ endmodule
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "coswave_fs64fsig_64pts.mif"
+// Retrieval info: CONSTANT: INIT_FILE STRING "sinwave_fs64fsig_64pts.mif"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- mem_demo/mem_demo.v
+++ mem_demo/mem_demo.v
@@ -5,25 +5,25 @@ module mem_demo(clk, ce, sclr, Q);
 input clk, ce, sclr;
 output [15:0] Q;
 wire actr_sclr;
-wire clk_div;
+//wire clk_div;
 wire [7:0] actr_Q;
 wire [8:0] mem_addr;
 wire [15:0] mem_Q;
 
-clkdiv CLK32768 (.inclk0 (clk),
-						.c0 (clk_div));
+//clkdiv CLK32768 (.inclk0 (clk),
+//						.c0 (clk_div));
 
 in_actr CTR ( .clk_en(ce),
-					.clock(clk_div),
+					.clock(clk),
 					.sclr(actr_sclr),
 					.q(actr_Q)
 					);
 					
-assign actr_sclr = ~sclr || actr_Q[6];		
+assign actr_sclr = ~sclr || ( actr_Q >= 8'b00111111);		
 assign mem_addr = {1'b0, actr_Q};	
 
 in_mem MEM ( .address (mem_addr),
-					.clock(clk_div),
+					.clock(clk),
 					.rden(ce),
 					.q(mem_Q)
 					);
